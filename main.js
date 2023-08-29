a4721569df5fc6893123f2ed0a3453a2873ea4ea
anisbhsl <bhusal.anish12@gmail.com>
2023-08-28 22:36:21 -0500

    some changes in UI

--- main.js
+++ main.js
@@ -39,3 +39,62 @@ map.on('style.load', () => {
       });
 
 }); 
+
+// change theme based upon radio button selection
+const themeList = document.getElementById('theme');
+const themeInputs = document.getElementsByTagName('input');
+const selectedYear = document.getElementById('select-year');
+
+
+//whenever HTML window is refreshed, the default theme will be changed to
+// satellite view 
+window.onload = function(){
+  map.setStyle('mapbox://styles/mapbox/satellite-streets-v12');
+}
+
+
+for (const input of themeInputs) {
+  input.onclick = (layer) => {
+  const layerId = layer.target.id;
+  map.setStyle('mapbox://styles/mapbox/' + layerId);
+};
+}
+
+
+//load hurricane list on the side 
+selectedYear.onclick = (layer)=>{
+    var selectedYearValue=layer.target.value;
+    console.log(selectedYearValue);
+
+    cards = `
+        <div class="hurricane-card">
+          <div class="hurricane-card-content">
+            <span>Katrina</span>
+            <p>Year: 2005</p>
+          </div>
+        </div>
+
+        <div class="hurricane-card">
+          <div class="hurricane-card-content">
+            <span>Katrina</span>
+            <p>Year: 2005</p>
+          </div>
+        </div>
+
+        <div class="hurricane-card">
+          <div class="hurricane-card-content">
+            <span>Katrina</span>
+            <p>Year: 2005</p>
+          </div>
+        </div>
+
+        <div class="hurricane-card">
+          <div class="hurricane-card-content">
+            <span>Katrina</span>
+            <p>Year: 2005</p>
+          </div>
+        </div>
+    `
+
+    document.getElementById("hurricane-list-container").innerHTML=cards;
+};
