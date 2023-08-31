30cbe7b07e8c5afb1b03bf9c4c23df363e4f02ec
Raj Dangol <rd0077@uah.edu>
2023-08-31 00:29:49 -0500

    added form submission; separated card js

--- main.js
+++ main.js
@@ -2,58 +2,6 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGFyaWRoaTEyIiwiYSI6ImNsaWMxcnRwejBnYXkzZG1ub
 var colorStops = ["#000000", "#222", "#ffc300", "#ff8d19", "#ff5733", "#ff2e00"]; 
 
 
-const container = document.querySelector('#cards-div');
-// Fetch data from the JSON file
-// TOOD: replace this
-fetch('jsonData.json')
-    .then(response => response.json())
-    .then(jsonData => {
-        jsonData.forEach((cardData, index) => {
-            const cardDiv = document.createElement('div');
-            cardDiv.classList.add('col-md-10', 'offset-md-1');
-
-            const cardContent = `
-            <div class="card border-primary mb-3">
-            <div class="card-header" data-bs-toggle="collapse" data-bs-target="#collapse${index}">
-                ${cardData.name}
-                <i class="fas fa-chevron-down float-right"></i>
-            </div>
-            <div class="card-body text-primary">
-                ${cardData.details.replace('\n', '<br>')}
-            </div>
-            <div class="collapse" id="collapse${index}">
-                <div class="card-body">
-                    <form>
-                        <div class="mb-3">
-                            <label for="windValue${index}" class="form-label">Wind Value</label>
-                            <input type="text" class="form-control" id="windValue${index}" placeholder="Enter wind value">
-                        </div>
-                        <div class="mb-3">
-                            <label for="categoryDropdown${index}" class="form-label">Category</label>
-                            <select class="form-select" id="categoryDropdown${index}">
-                                <option selected>Choose category...</option>
-                                <option value="1">Category 1</option>
-                                <option value="2">Category 2</option>
-                                <option value="3">Category 3</option>
-                                <!-- Add other categories as needed -->
-                            </select>
-                        </div>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#iframeModal">Show Plot</button>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-    `;
-
-
-            cardDiv.innerHTML = cardContent;
-            container.appendChild(cardDiv);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching JSON:', error);
-    });
 
 // Set the height of hurricane cards div
 const height = document.querySelector(".col-9").offsetHeight;
